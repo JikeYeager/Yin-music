8d4ddef86386abbf43db2d12c3b7b82c3c4a3552
18673329363 <xmli99@qq.com>
2025-09-23 18:32:21 +0800

    Revert "第五部分：歌手和歌单模块"
    This reverts commit 0371c6b04ecc19b6865036689b01878726f16d50.

--- src/main/java/com/ly/yin/mapper/SingerMapper.java
+++
@@ -1,8 +0,0 @@
-package com.ly.yin.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.ly.yin.domain.Singer;
-
-public interface SingerMapper extends BaseMapper<Singer>
-{
-}
--- src/main/java/com/ly/yin/mapper/SongListMapper.java
+++
@@ -1,7 +0,0 @@
-package com.ly.yin.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.ly.yin.domain.SongList;
-public interface SongListMapper extends BaseMapper<SongList>
-{
-}
